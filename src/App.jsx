3732581cb4b63aa040d82f02d20e05b2dcf7bb8f
elinjakobsen <126721502+elinkrull@users.noreply.github.com>
2025-04-29 15:06:39 +0200

    include routes

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,13 @@
-import React from "react";
+import { Routes, Route } from "react-router-dom";
 import Home from "./pages/Home";
+import VenueDetails from "./pages/VenueDetails";
 
 function App() {
   return (
-    <>
-      <Home />
-    </>
+    <Routes>
+      <Route path="/" element={<Home />} />
+      <Route path="/venue/:id" element={<VenueDetails />} />
+    </Routes>
   );
 }
 
